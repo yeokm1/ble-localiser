8f7bdd956be496094be6aa0002697113c846b301
Yeo Kheng Meng <yeokm1@gmail.com>
2017-09-10 23:41:14 +0800

    manual colour control

--- BLE Locator/BLE Locator/ManualVC.swift
+++ BLE Locator/BLE Locator/ManualVC.swift
@@ -10,6 +10,9 @@ import Foundation
 import UIKit
 import ChromaColorPicker
 
+let colourRange = 255.0
+let maxBrightness = 10.0
+
 class ManualVC: UIViewController{
     
     var piComm: PiComm!
@@ -18,7 +21,13 @@ class ManualVC: UIViewController{
     var middleColorPicker: ChromaColorPicker!
     var rightColorPicker: ChromaColorPicker!
     
-    let colourRange = 255.0
+    //red, green, blue and numLEDs state
+    var lastLeftState = [colourRange, colourRange, colourRange, 8]
+    var lastMiddleState = [colourRange, colourRange, colourRange, 8]
+    var lastRightState = [colourRange, colourRange, colourRange, 8]
+    
+
+
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
@@ -28,13 +37,13 @@ class ManualVC: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        leftColorPicker = createColorPicker(xPos: 5, yPos: 10, valueChangedSelector: #selector(leftColourPickerValueChanged))
+        leftColorPicker = createColorPicker(xPos: 3, yPos: 10, valueChangedSelector: #selector(leftColourPickerValueChanged))
         view.addSubview(leftColorPicker)
         
-        middleColorPicker = createColorPicker(xPos: 195, yPos: 10, valueChangedSelector: #selector(middleColourPickerValueChanged))
+        middleColorPicker = createColorPicker(xPos: 190, yPos: 10, valueChangedSelector: #selector(middleColourPickerValueChanged))
         view.addSubview(middleColorPicker)
         
-        rightColorPicker = createColorPicker(xPos: 385, yPos: 10, valueChangedSelector: #selector(rightColourPickerValueChanged))
+        rightColorPicker = createColorPicker(xPos: 373, yPos: 10, valueChangedSelector: #selector(rightColourPickerValueChanged))
         view.addSubview(rightColorPicker)
         
         piComm = PiComm()
@@ -42,45 +51,83 @@ class ManualVC: UIViewController{
     }
     
     func createColorPicker(xPos: Int, yPos: Int, valueChangedSelector: Selector) -> ChromaColorPicker{
-        let colorPicker = ChromaColorPicker(frame: CGRect(x: xPos, y: yPos, width: 175, height: 175))
+        let colorPicker = ChromaColorPicker(frame: CGRect(x: xPos, y: yPos, width: 185, height: 185))
         colorPicker.padding = 0
         colorPicker.stroke = 15
         colorPicker.hexLabel.isHidden = true
+        colorPicker.shadeSlider.isHidden = true
         colorPicker.addTarget(self, action: valueChangedSelector, for: UIControlEvents.valueChanged)
         return colorPicker
         
     }
     
 
-    
     func leftColourPickerValueChanged(){
         let rgbColour = convertUIColorTo8bitRGB(color: leftColorPicker.currentColor)
+        
+        lastLeftState[0] = rgbColour.r
+        lastLeftState[1] = rgbColour.g
+        lastLeftState[2] = rgbColour.b
+        
         print("Left: \(rgbColour)")
+        updateLeftToNetwork()
     }
     
     func middleColourPickerValueChanged(){
         let rgbColour = convertUIColorTo8bitRGB(color: middleColorPicker.currentColor)
+        
+        lastMiddleState[0] = rgbColour.r
+        lastMiddleState[1] = rgbColour.g
+        lastMiddleState[2] = rgbColour.b
+        
         print("Middle: \(rgbColour)")
+        updateMiddleToNetwork()
     }
     
     func rightColourPickerValueChanged(){
         let rgbColour = convertUIColorTo8bitRGB(color: rightColorPicker.currentColor)
+        
+        lastRightState[0] = rgbColour.r
+        lastRightState[1] = rgbColour.g
+        lastRightState[2] = rgbColour.b
+        
         print("Right: \(rgbColour)")
+        updateRightToNetwork()
+    }
+    
+    func updateLeftToNetwork(){
+        prepareToSendToNetwork(id: greenMacAddress, ledsToTurnOn: Int(lastLeftState[3]), red: lastLeftState[0], green: lastLeftState[1], blue: lastLeftState[2])
+    }
+    
+    func updateMiddleToNetwork(){
+        prepareToSendToNetwork(id: redMacAddress, ledsToTurnOn: Int(lastMiddleState[3]), red: lastMiddleState[0], green: lastMiddleState[1], blue: lastMiddleState[2])
+    }
+    
+    func updateRightToNetwork(){
+        prepareToSendToNetwork(id: blueMacAddress, ledsToTurnOn: Int(lastRightState[3]), red: lastRightState[0], green: lastRightState[1], blue: lastRightState[2])
     }
     
     
-    func convertUIColorTo8bitRGB(color: UIColor) -> (r: Int, g: Int, b:Int){
+    func convertUIColorTo8bitRGB(color: UIColor) -> (r: Double, g: Double, b:Double){
         let cgColor = color.cgColor
         let redFloat = Double(cgColor.components![0])
         let greenFloat = Double(cgColor.components![1])
         let blueFloat = Double(cgColor.components![2])
         
-        let red: Int = Int(colourRange * redFloat)
-        let green: Int = Int(colourRange * greenFloat)
-        let blue: Int = Int(colourRange * blueFloat)
+        let red = colourRange * redFloat
+        let green = colourRange * greenFloat
+        let blue = colourRange * blueFloat
         
         return (r: red, g: green, b: blue)
+    }
+    
+    func prepareToSendToNetwork(id: String, ledsToTurnOn: Int, red: Double, green: Double, blue: Double){
         
+        let newRed = Int((red / colourRange) * maxBrightness)
+        let newGreen = Int((green / colourRange) * maxBrightness)
+        let newBlue = Int((blue / colourRange) * maxBrightness)
+        
+        piComm.sendPacket(id: id, ledsToTurnOn: ledsToTurnOn, red: newRed, green: newGreen, blue: newBlue)
     }
     
     
