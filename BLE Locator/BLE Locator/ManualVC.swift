e72abdfdeab92f381b96355be53ab1ed79101bac
Yeo Kheng Meng <yeokm1@gmail.com>
2017-09-16 18:20:59 +0800

    brightness slider

--- BLE Locator/BLE Locator/ManualVC.swift
+++ BLE Locator/BLE Locator/ManualVC.swift
@@ -12,13 +12,13 @@ import ChromaColorPicker
 
 let colourRange = 255.0
 
-let LEDs_MIN = 0
-let LEDs_DEFAULT = 32.0
-let LEDS_MAX = 64.0
+let LEDs_MIN: Float = 0.0
+let LEDs_DEFAULT: Float = 32.0
+let LEDS_MAX: Float = 64.0
 
-let BRIGHT_MIN = 0
-let BRIGHT_DEFAULT = 50.0
-let BRIGHT_MAX = 255
+let BRIGHT_MIN: Float = 0.0
+let BRIGHT_DEFAULT: Float = 50.0
+let BRIGHT_MAX: Float = 255
 
 
 
@@ -33,14 +33,17 @@ class ManualVC: UIViewController{
     var leftNumLEDSlider: UISlider!
     var leftNumLEDLabel: UILabel!
     
+    var leftBrightSlider: UISlider!
+    var leftBrightLabel: UILabel!
+    
     var leftBrightnessSlider: UISlider!
     var rightBrightnessSlider: UILabel!
 
     
     //red, green, blue and numLEDs state
-    var lastLeftState: [Double] = [0, 0, 0, LEDs_DEFAULT, BRIGHT_DEFAULT]
-    var lastMiddleState: [Double] = [0, 0, 0, LEDs_DEFAULT, BRIGHT_DEFAULT]
-    var lastRightState: [Double] = [0, 0, 0, LEDs_DEFAULT, BRIGHT_DEFAULT]
+    var lastLeftState: [Float] = [0, 0, 0, LEDs_DEFAULT, BRIGHT_DEFAULT]
+    var lastMiddleState: [Float] = [0, 0, 0, LEDs_DEFAULT, BRIGHT_DEFAULT]
+    var lastRightState: [Float] = [0, 0, 0, LEDs_DEFAULT, BRIGHT_DEFAULT]
 
 
     
@@ -61,11 +64,19 @@ class ManualVC: UIViewController{
         rightColorPicker = createColorPicker(xPos: 320, yPos: 10, valueChangedSelector: #selector(rightColourPickerValueChanged))
         view.addSubview(rightColorPicker)
         
-        let leftNumLEDSliderAndLabel = createSliderAndLabel(xPos: 3, yPos: 200, ledMinValue: 0, ledMaxValue: 64, ledCurrentValue: 32, ledValueChangedSelector: #selector(leftLEDNumSliderValueChanged))
-        leftNumLEDSlider = leftNumLEDSliderAndLabel.slider
-        leftNumLEDLabel = leftNumLEDSliderAndLabel.label
+        let leftSliderAndLabel = createSlidersAndLabels(xPos: 3, yPos: 175, ledMinValue: LEDs_MIN, ledMaxValue: LEDS_MAX, ledCurrentValue: LEDs_DEFAULT, ledValueChangedSelector: #selector(leftLEDNumSliderValueChanged), brightMinValue: BRIGHT_MIN, brightMaxValue: BRIGHT_MAX, brightCurrentValue: BRIGHT_DEFAULT, brightValueChangedSelector: #selector(leftBrightSlidervalueChanged))
+        
+        
+        leftNumLEDSlider = leftSliderAndLabel.ledNumSlider
+        leftNumLEDLabel = leftSliderAndLabel.ledNumLabel
+        
+        leftBrightSlider = leftSliderAndLabel.brightNumSlider
+        leftBrightLabel = leftSliderAndLabel.brightNumLabel
+        
         view.addSubview(leftNumLEDSlider)
         view.addSubview(leftNumLEDLabel)
+        view.addSubview(leftBrightSlider)
+        view.addSubview(leftBrightLabel)
         
     }
     
@@ -87,23 +98,49 @@ class ManualVC: UIViewController{
     }
     
     
-    func createSliderAndLabel(xPos: Int, yPos: Int, ledMinValue: Float, ledMaxValue: Float, ledCurrentValue: Float, ledValueChangedSelector: Selector) -> (slider: UISlider, label: UILabel){
+    func createSlidersAndLabels(xPos: Int,
+                                yPos: Int,
+                                ledMinValue: Float,
+                                ledMaxValue: Float,
+                                ledCurrentValue: Float,
+                                ledValueChangedSelector: Selector,
+                                
+                                brightMinValue: Float,
+                                brightMaxValue: Float,
+                                brightCurrentValue: Float,
+                                brightValueChangedSelector: Selector)
+        -> (ledNumSlider: UISlider, ledNumLabel: UILabel, brightNumSlider: UISlider, brightNumLabel: UILabel){
         
-        let label = UILabel(frame: CGRect(x: xPos, y: yPos, width: 30, height: 50))
+        let ledNumLabel = UILabel(frame: CGRect(x: xPos, y: yPos, width: 35, height: 50))
         
         // We want to strip away the decimal point
-        label.text = String(Int(ledCurrentValue))
+        ledNumLabel.text = String(Int(ledCurrentValue))
         
-        let slider = UISlider(frame: CGRect(x: xPos + 30, y: yPos, width: 90, height: 50))
+        let ledNumSlider = UISlider(frame: CGRect(x: xPos + 30, y: yPos, width: 90, height: 50))
         
-        slider.backgroundColor = UIColor.clear
-        slider.minimumValue = ledMinValue;
-        slider.maximumValue = ledMaxValue;
-        slider.isContinuous = true;
-        slider.value = ledCurrentValue;
-        slider.addTarget(self, action: ledValueChangedSelector, for: .valueChanged)
+        ledNumSlider.backgroundColor = UIColor.clear
+        ledNumSlider.minimumValue = ledMinValue;
+        ledNumSlider.maximumValue = ledMaxValue;
+        ledNumSlider.isContinuous = true;
+        ledNumSlider.value = ledCurrentValue;
+        ledNumSlider.addTarget(self, action: ledValueChangedSelector, for: .valueChanged)
         
-        return (slider: slider, label: label)
+        
+        let brightNumLabel = UILabel(frame: CGRect(x: xPos, y: yPos + 50, width: 35, height: 50))
+        
+        // We want to strip away the decimal point
+        brightNumLabel.text = String(Int(brightCurrentValue))
+        
+        let brightNumSlider = UISlider(frame: CGRect(x: xPos + 30, y: yPos + 50, width: 90, height: 50))
+        
+        brightNumSlider.backgroundColor = UIColor.clear
+        brightNumSlider.minimumValue = brightMinValue;
+        brightNumSlider.maximumValue = brightMaxValue;
+        brightNumSlider.isContinuous = true;
+        brightNumSlider.value = brightCurrentValue;
+        brightNumSlider.addTarget(self, action: brightValueChangedSelector, for: .valueChanged)
+        
+        return (ledNumSlider: ledNumSlider, ledNumLabel: ledNumLabel, brightNumSlider: brightNumSlider, brightNumLabel: brightNumLabel)
     }
     
     func createColorPicker(xPos: Int, yPos: Int, valueChangedSelector: Selector) -> ChromaColorPicker{
@@ -152,9 +189,16 @@ class ManualVC: UIViewController{
     }
     
     func leftLEDNumSliderValueChanged(sender: UISlider){
-        lastLeftState[3] = Double(sender.value)
+        lastLeftState[3] = Float(sender.value)
         leftNumLEDLabel.text = String(Int(lastLeftState[3]))
         updateLeftToNetwork()
+    }
+    
+    func leftBrightSlidervalueChanged(sender: UISlider){
+        lastLeftState[4] = Float(sender.value)
+        leftBrightLabel.text = String(Int(lastLeftState[4]))
+        updateLeftToNetwork()
+        
     }
     
     func updateLeftToNetwork(){
@@ -170,16 +214,16 @@ class ManualVC: UIViewController{
     }
     
     
-    func convertUIColorToRGB(color: UIColor) -> (r: Double, g: Double, b:Double){
+    func convertUIColorToRGB(color: UIColor) -> (r: Float, g: Float, b: Float){
         let cgColor = color.cgColor
-        let red = Double(cgColor.components![0])
-        let green = Double(cgColor.components![1])
-        let blue = Double(cgColor.components![2])
+        let red = Float(cgColor.components![0])
+        let green = Float(cgColor.components![1])
+        let blue = Float(cgColor.components![2])
         
         return (r: red, g: green, b: blue)
     }
     
-    func prepareToSendToNetwork(id: String, ledsToTurnOn: Int, red: Double, green: Double, blue: Double, brightness: Double){
+    func prepareToSendToNetwork(id: String, ledsToTurnOn: Int, red: Float, green: Float, blue: Float, brightness: Float){
 
         let newRed = Int(red * brightness)
         let newGreen = Int(green * brightness)
